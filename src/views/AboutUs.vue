8be6f920c1eb24cc6001e12bb8000ff82f7cd5d7
Frezga <frezga3@gmail.com>
2025-06-14 13:58:55 +0700

    yy

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -3,21 +3,27 @@
     <div class="about-illustration">
       <Ilustrasi />
     </div>
-    <div class="about-glass-box" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600" data-aos-once="true">
+    <div
+      class="about-glass-box"
+      data-aos="fade-up"
+      data-aos-duration="1000"
+      data-aos-delay="600"
+      data-aos-once="true"
+    >
       <h2 class="about-title">About Us</h2>
       <p>
-        Kami adalah kelompok dengan nomor urut ke sepuluh (berdasarkan urutan pada <span class="highlight">spreadsheet</span> yang ada di google classroom)
+        Kami adalah kelompok dengan nomor urut ke sepuluh (berdasarkan urutan pada
+        <span class="highlight">spreadsheet</span> yang ada di google classroom)
       </p>
+      <div>Anggota kelompok kami terdiri dari:</div>
+      <ul>
+        <li>• Muhamad Afif Aji Putra (L0124134)</li>
+        <li>• Muhammad Akbar Kurniawan (L0124136)</li>
+        <li>• Muhammad Ayman (L0124137)</li>
+      </ul>
       <p>
-        Anggota kelompok kami terdiri dari:
-        <ul>
-          <li>• Muhamad Afif Aji Putra (L0124134)</li>
-          <li>• Muhammad Akbar Kurniawan (L0124136)</li>
-          <li>• Muhammad Ayman (L0124137)</li>
-        </ul>
-      </p>
-      <p>
-        Saat mengerjakan project ini, kami sedang menempuh semester dua pada program studi Informatika (Fakultas Teknologi Informasi dan Sains Data) di Universitas Sebelas Maret.
+        Saat mengerjakan project ini, kami sedang menempuh semester dua pada program studi
+        Informatika (Fakultas Teknologi Informasi dan Sains Data) di Universitas Sebelas Maret.
       </p>
     </div>
   </section>
